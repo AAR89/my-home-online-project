46219c7b7236bd959b65540c1496bf63f5d26602
Alex Ur <ru.booking@yandex.ru>
2024-11-28 16:58:46 +0300

    add sticky pagination

--- src/views/AppealsView.vue
+++ src/views/AppealsView.vue
@@ -1,68 +1,83 @@
 <template>
   <div class="appeals">
     <h1 class="appeals-title">Список заявок</h1>
-    <section class="table-section">
-      <section class="create-section">
-        <button class="create-appeal-button" @click="openModal(null)">
-          СОЗДАТЬ
-        </button>
-      </section>
-      <table>
-        <thead>
-          <tr>
-            <th>№</th>
-            <th>Создана</th>
-            <th>Адрес</th>
-            <th>Заявитель</th>
-            <th>Описание</th>
-            <th>Срок</th>
-            <th>Статус</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="appeal in appeals"
-            :key="appeal.id"
-            @click="openModal(appeal)"
-          >
-            <section class="td-number-section">
-              <button class="number-button">{{ appeal.number }}</button>
-            </section>
-            <td>{{ formatDate(appeal.created_at) }}</td>
-            <td>
-              {{
-                appeal.apartment?.label
-                  ? appeal.apartment.label
-                  : "нет информации"
-              }}
-            </td>
-            <td>{{ appeal.applicant ? appeal.applicant?.username : "" }}</td>
-            <td class="description">
-              {{ truncateText(appeal.description, 30) }}
-              <span
-                v-if="appeal.description.length > 30"
-                class="description-text"
-              >
-                {{ appeal.description }}
-              </span>
-            </td>
-            <td>{{ formatDate(appeal.due_date) }}</td>
-            <td
-              :class="{
-                'red-status': appeal.status.is_red_details,
-                'green-status': !appeal.status.is_red_details,
-              }"
-              class="status-cell"
+    <div class="table-pagination-wrapper">
+      <section class="table-section">
+        <section class="create-section">
+          <button class="create-appeal-button" @click="openModal(null)">
+            СОЗДАТЬ
+          </button>
+        </section>
+        <table>
+          <thead>
+            <tr>
+              <th>№</th>
+              <th>Создана</th>
+              <th>Адрес</th>
+              <th>Заявитель</th>
+              <th>Описание</th>
+              <th>Срок</th>
+              <th>Статус</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="appeal in paginatedAppeals"
+              :key="appeal.id"
+              @click="openModal(appeal)"
             >
-              {{ appeal.status.name }}
-              <span v-if="appeal.status.is_red_details" class="tooltip">
-                {{ appeal.status.short_details }}
-              </span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
+              <section class="td-number-section">
+                <button class="number-button">{{ appeal.number }}</button>
+              </section>
+              <td>{{ formatDate(appeal.created_at) }}</td>
+              <td>
+                {{ "нет информации" || appeal.apartment?.label }}
+              </td>
+              <td>{{ appeal.applicant ? appeal.applicant?.username : "" }}</td>
+              <td class="description">
+                {{ truncateText(appeal.description, 30) }}
+                <span
+                  v-if="appeal.description.length > 30"
+                  class="description-text"
+                >
+                  {{ appeal.description }}
+                </span>
+              </td>
+              <td>{{ formatDate(appeal.due_date) }}</td>
+              <td
+                :class="{
+                  'red-status': appeal.status.is_red_details,
+                  'green-status': !appeal.status.is_red_details,
+                }"
+                class="status-cell"
+              >
+                {{
+                  appeal.status.is_red_details
+                    ? "Просрочено"
+                    : appeal.status.name
+                }}
+                <span v-if="appeal.status.is_red_details" class="tooltip">
+                  {{ appeal.status.short_details }}
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <!-- <div class="pagination-wrapper"> -->
+        <div class="pagination">
+          <div>
+            <button @click="prevPage" :disabled="currentPage === 1">
+              Назад
+            </button>
+            <span>Страница {{ currentPage }} из {{ totalPages }}</span>
+            <button @click="nextPage" :disabled="currentPage === totalPages">
+              Вперед
+            </button>
+          </div>
+        </div>
+        <!-- </div> -->
+      </section>
+    </div>
     <appeal-modal
       :is-visible="isModalVisible"
       :is-edit-mode="isEditMode"
@@ -88,25 +103,49 @@ export default {
       isModalVisible: false,
       isEditMode: false,
       appealToEdit: null,
+      currentPage: 1,
+      itemsPerPage: 10,
     };
   },
   computed: {
     ...mapState(["appeals"]),
+    totalPages() {
+      return Math.ceil(this.appeals.length / this.itemsPerPage);
+    },
+    paginatedAppeals() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      return this.appeals.slice(start, start + this.itemsPerPage);
+    },
   },
   methods: {
     ...mapActions(["fetchAppeals", "createAppeal", "updateAppeal"]),
+
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+
     truncateText(text, maxLength) {
       if (!text) return "нет информации";
       return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
     },
+
     formatDate(dateString) {
       if (!dateString) return "нет информации";
       return format(new Date(dateString), "dd.MM.yyyy");
     },
+
     openModal(appeal) {
       if (appeal) {
         this.isEditMode = true;
-        this.appealToEdit = appeal; // Передаем только объект заявки
+        this.appealToEdit = appeal;
       } else {
         this.isEditMode = false;
         this.appealToEdit = {
@@ -118,28 +157,28 @@ export default {
           description: "",
           due_date: "",
           status: "Новый",
-        }; // Новый пустой объект для создания
+        };
       }
       this.isModalVisible = true;
     },
+
     closeModal() {
       this.isEditMode = false;
       this.isModalVisible = false;
     },
+
     async handleSave(updatedAppeal) {
       if (this.isEditMode) {
-        // Обновление существующей заявки
         await this.updateAppeal(updatedAppeal);
       } else {
-        // Создание новой заявки
         await this.createAppeal(updatedAppeal);
       }
       this.closeModal();
-      this.fetchAppeals(); // Обновляем список заявок после сохранения
+      this.fetchAppeals();
     },
   },
   created() {
-    this.fetchAppeals(); // Загружаем список заявок при монтировании компонента
+    this.fetchAppeals();
   },
 };
 </script>
@@ -147,8 +186,10 @@ export default {
 <style scoped lang="scss">
 .appeals {
   background-color: #eeeeee;
-  width: 1350px;
-  padding: 23px 15px 0px 15px;
+  width: 100vw;
+  padding: 23px 15px 15px 15px;
+  position: relative;
+  box-sizing: border-box;
 
   .appeals-title {
     margin: 0px 0px 40px 32px;
@@ -159,6 +200,22 @@ export default {
     text-underline-position: from-font;
     text-decoration-skip-ink: none;
     color: #454545;
+  }
+
+  .table-pagination-wrapper {
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+
+  .table-section {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-content: space-between;
+
+    box-shadow: 0 -2px 5px rgba(105, 103, 103, 0.1);
   }
 
   .table-section,
@@ -364,5 +421,40 @@ export default {
     visibility: visible;
     opacity: 1;
   }
+
+  // .pagination-wrapper {
+  //   position: sticky;
+  //   bottom: 0;
+  //   left: 0;
+  //   width: 100%;
+  //   background-color: white;
+  //   padding: 0 15px;
+  //   box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+
+  .pagination {
+    position: sticky;
+    bottom: 0;
+    background-color: white;
+    display: flex;
+    padding: 10px 20px 10px 20px;
+    justify-content: space-between;
+    box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+  }
+
+  .pagination button {
+    padding: 5px 15px;
+    margin: 0 5px;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .pagination button:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
 }
+// }
 </style>
